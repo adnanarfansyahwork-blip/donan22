302b74ff6f69e0b2adbccdbe1084917a28e8ceab
adnanarfansyah7-sys <adnanarfansyah7@gmail.com>
2026-01-02 19:12:26 +0700

    Add cron setup documentation and scheduled task examples

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Scheduled Tasks
+Schedule::call(function () {
+    \Log::info('Cron job running at ' . now());
+})->everyMinute()->name('test-cron');
+
+// Example: Clear old view cache daily
+Schedule::command('view:clear')->daily()->at('02:00');
+
+// Example: Generate sitemap weekly
+// Schedule::command('sitemap:generate')->weekly()->sundays()->at('03:00');
